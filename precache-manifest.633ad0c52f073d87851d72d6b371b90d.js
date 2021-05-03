1a64acbcdc2caaabcceb30ab139f0cf21457ed4e
KyoukanPT <kyoukanpt@gmail.com>
2021-05-03 06:46:35 +0000

    Updates

---
+++ precache-manifest.633ad0c52f073d87851d72d6b371b90d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df75c8f58fefd91200886d179394bab",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "fde524a40d070b147e3b",
+    "url": "/keeper-app/static/js/2.c9cc8fed.chunk.js"
+  },
+  {
+    "revision": "060e5f5c62f403171b02",
+    "url": "/keeper-app/static/js/main.0e73b29d.chunk.js"
+  },
+  {
+    "revision": "955777ecbb97c21b1562",
+    "url": "/keeper-app/static/js/runtime-main.2b705169.js"
+  }
+]);
